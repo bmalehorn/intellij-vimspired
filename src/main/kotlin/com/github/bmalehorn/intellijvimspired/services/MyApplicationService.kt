854683c626cb0e5871ed70fc173cee4e4c523a62
GitHub Action <action@github.com>
2021-05-15 20:09:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bmalehorn/intellijvimspired/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bmalehorn.intellijvimspired.services
+
+import com.github.bmalehorn.intellijvimspired.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bmalehorn/intellijvimspired/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bmalehorn/intellijvimspired/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bmalehorn.intellijvimspired.services
 
+import com.github.bmalehorn.intellijvimspired.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
